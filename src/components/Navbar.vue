2e8d713f499b3b69a34d727a3aeb51ca67348319
ron9295 <43638298+ron9295@users.noreply.github.com>
2022-08-25 22:40:19 +0300

    Add files via upload

---
+++ src/components/Navbar.vue
@@ -0,0 +1,38 @@
+<template>
+  <nav class="navbar navbar-costum">
+    <div class="container-fluid">
+      <span class="navbar-brand mb-0 h1">Glamour</span>
+    </div>
+    <div dir="RTL">
+      <ul class="navbar-nav me-auto mb-2">
+        <li class="nav-item">
+          <router-link to="/" class="nav-link" exact>בית</router-link>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  props: {},
+};
+</script>
+
+<style scoped>
+.navbar-costum {
+  height: 10vh;
+  direction: rtl;
+  background-color: rgb(149, 125, 173);
+}
+span {
+  color: black;
+}
+a {
+  color: white;
+}
+.link-list {
+  direction: rtl;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="home">
+    <Navbar />
+    <div class="form-big">
+      <div class="row">
+        <div class="col-6">
+          <button type="button" class="btn btn-costum" @click="$router.push('/register')">
+            להרשמה
+          </button>
+        </div>
+      </div>
+      <div class="row mt-3">
+        <div class="col-6">
+          <button type="button" class="btn btn-costum">להתחברות</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Navbar from "../components/Navbar.vue";
+
+export default {
+  name: "HomeView",
+  components: {
+    Navbar,
+  },
+};
+</script>
+
+<style scoped>
+.btn-costum {
+  width: 40vw;
+  height: 8vh;
+  color: white;
+  background-color: rgb(210, 145, 188);
+  font-size: 22px;
+  font-family: Arial, Helvetica, sans-serif;
+}
+.form-big {
+  background-color: rgb(255, 223, 211);
+  flex-direction: column;
+  height: 90vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
